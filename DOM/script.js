1dd143d348ef1d0fefe0b27b274e6855572e3f2f
Wagner Carvalho <wagncarv@gmail.com>
2021-02-18 14:44:36 -0300

    js

---
+++ DOM/script.js
@@ -0,0 +1,10 @@
+//
+// let titulo = document.getElementById("titulo")
+// console.log(titulo)
+
+// let title = document.querySelector("#titulo")
+// console.log(title)
+
+let elements = document.querySelectorAll("meta")
+elements.forEach(el => console.log(el.name))
+// console.log(elements)
