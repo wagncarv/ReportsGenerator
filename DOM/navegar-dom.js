d7f201dca3f7ecb1925514abeb03543e493f8f23
Wagner Carvalho <wagncarv@gmail.com>
2021-02-18 17:21:59 -0300

    adicionar elementos DOM

---
+++ DOM/navegar-dom.js
@@ -0,0 +1,31 @@
+
+//Navegando pelos elementos
+//parentNode e parentElement
+// const body = document.querySelector("body")
+// console.log(body.parentNode)
+
+//childNodes e children
+// const el = document.querySelector("body")
+// console.log(el.children)
+// console.log(el.childNodes)
+
+//firstChild firstElementChild
+// console.log(el.firstChild)
+// console.log(el.firstElementChild)
+
+//lastChild lastElementChild
+// console.log(el.lastChild)
+// console.log(el.lastElementChild)
+
+
+//
+//
+const el = document.querySelector("header")
+//nextSibling nextElementChild
+console.log(el.nextSibling)
+console.log(el.nextElementSibling)
+
+//previousSibling previousElementSibling
+console.log(el.previousSibling)
+console.log(el.previousElementSibling)
+
--- DOM/script.js
+++ DOM/script.js
@@ -5,6 +5,45 @@
 // let title = document.querySelector("#titulo")
 // console.log(title)
 
-let elements = document.querySelectorAll("meta")
-elements.forEach(el => console.log(el.name))
+// let elements = document.querySelectorAll("meta")
+// elements.forEach(el => console.log(el.name))
 // console.log(elements)
+
+//innerText
+// const el = document.getElementById("titulo")
+// el.innerText = "Olá devs"
+
+//MANIPULAR CAMPOS HTML
+// const el = document.querySelector("input")
+// el.value = "Valor qualquer"
+
+//MANIPULAR ATRIBUTOS
+// const header = document.querySelector("header")
+// header.setAttribute("id", "header")
+
+// const headerid = document.querySelector("#header")
+// console.log(headerid)
+// console.log(headerid.getAttribute("id"))
+
+//
+// console.log(headerid.getAttribute("class"))
+
+// header.removeAttribute("id")
+// headerid.removeAttribute("class")
+
+// header.setAttribute("class", "bg header")
+
+
+//ALTERANDO ESTILOS classList e style
+// const el = document.querySelector("body")
+// el.style.backgroundColor = "#00ff00"
+// console.log(el.style.backgroundColor)
+
+//CLASSlist
+const el = document.querySelector("body")
+el.classList.add("active", "green")
+console.log(el.classList)
+
+// el.classList.remove("active")
+//toggle = se classe existir, tira, se não, põe
+el.classList.toggle("active")
