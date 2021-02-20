177a35bd05e6c300e2aa8d842cf25f8a9bba0a7d
Wagner Carvalho <wagncarv@gmail.com>
2021-02-20 16:06:43 -0300

    tipos de dados

--- js/tipodDeDados.js
+++ js/tipodDeDados.js
@@ -1,7 +1,36 @@
+////////////////////////////////////////////////////////////////
 function exec(value){
     const exec = document.querySelector("#exec")
-    exec.innerHTML = value
+    if(typeof value == "object" || typeof value == "function"){
+        exec.innerHTML = JSON.stringify(value)
+    }else{
+        exec.innerHTML = value
+    }
+
 }
 
 ////////////////////////////////////////////////////////////////
-exec("olá")
+// String
+// exec("olá")
+// exec(`soma ${1+1}`)
+
+//Number
+// exec(33)
+// exec(12.5/4.7)
+
+//Boolean
+// true
+// false
+
+//null && undefined
+// null
+// undefined
+
+//object
+const pessoa = {nome: "wagner carvalho", idade: 30, andar: function(){console.log("andar")}}
+// exec(pessoa)
+
+//Array
+const lista = ["pão", 10, "água", "arroz"]
+exec(lista)
+
